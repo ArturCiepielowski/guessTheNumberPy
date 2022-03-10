eda6fa104be865db28b4becac4f984841b3c5dfc
Artur Ciepielowski <artciepi@gmail.com>
2022-03-10 21:29:07 +0100

    Added function

--- guessNumber.py
+++ guessNumber.py
@@ -1,1 +1,18 @@
-print("Welcome to the Number Guessing Game!")
+import random
+print("Welcome to the Number Guessing Game!\nI am thinking of number between 1 and 100.")
+dificultyChoice = input("Choose dificlty. Type 'easy' or 'hard': ")
+
+
+def dificulty():
+    if dificultyChoice == "easy":
+        return 10
+    elif dificultyChoice == "hard":
+        return 5    
+
+
+
+game = True
+
+while game :
+    print("Game continues")
+
